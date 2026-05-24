8cd86df254436b52528c66dce1999654c60adc67
aunxxxx <fengaus0905@gmail.com>
2026-05-24 19:14:01 +0800

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,25 @@
+export const currentUser = {
+  name: "练习生001",
+  role: "trainee"
+};
+
+export const posts = [
+  {
+    user: "李",
+    name: "李四",
+    content: "第一位认证用户报到！大家好啊！",
+    image: "",
+    likes: 10,
+    comments: 0,
+    time: "刚刚"
+  },
+  {
+    user: "张",
+    name: "张三",
+    content: "契弟广场终于开了，开心！",
+    image: "",
+    likes: 2,
+    comments: 0,
+    time: "刚刚"
+  }
+];
