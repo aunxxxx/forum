7b544c2198ee9a42fd127289afbdff26c40dce95
aunxxxx <fengaus0905@gmail.com>
2026-05-25 21:36:49 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,27 +1,22 @@
-export const currentUser = {
-  name: "练习生001",
-  role: "trainee"
-};
-
 export const posts = [
   {
     id: "p1",
     user: "李",
     name: "李四",
-    content: "第一位认证用户报到！大家好啊！",
-    image: "",
-    likes: 10,
-    comments: 0,
-    time: "刚刚"
+    content: "第一条帖子",
+    time: "刚刚",
+    likes: 0,
+
+    // ⭐ 核心：点赞记录
+    likeMap: {}
   },
   {
     id: "p2",
     user: "张",
     name: "张三",
-    content: "契弟广场终于开了，开心！",
-    image: "",
-    likes: 2,
-    comments: 0,
-    time: "刚刚"
+    content: "第二条帖子",
+    time: "刚刚",
+    likes: 0,
+    likeMap: {}
   }
 ];
