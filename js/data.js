88c04beb123df006d5adc69248cd0e5ee60bc882
aunxxxx <fengaus0905@gmail.com>
2026-05-25 21:46:38 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,33 +1,25 @@
 export const currentUser = {
-    name: "练习生001",
-    role: "trainee" // ⭐ 只有 trainee 才能发帖
+  name: "练习生001",
+  role: "trainee"
 };
 
 export const posts = [
-    {
-        id: "p1",
-        user: "李",
-        name: "李四",
-        role: "trainee",
-        content: "第一位认证用户报到！大家好啊！",
-        image: "",
-        likes: 10,
-        comments: 0,
-        time: "刚刚",
-        likedBy: [
-            { id: "u1", name: "马睿婕", title: "创始", avatar: "https://i.pravatar.cc/40?img=3", count: 3 }
-        ]
-    },
-    {
-        id: "p2",
-        user: "张",
-        name: "张三",
-        role: "trainee",
-        content: "契弟广场终于开了，开心！",
-        image: "",
-        likes: 2,
-        comments: 0,
-        time: "刚刚",
-        likedBy: []
-    }
+  {
+    user: "李",
+    name: "李四",
+    content: "第一位认证用户报到！大家好啊！",
+    image: "",
+    likes: 10,
+    comments: 0,
+    time: "刚刚"
+  },
+  {
+    user: "张",
+    name: "张三",
+    content: "契弟广场终于开了，开心！",
+    image: "",
+    likes: 2,
+    comments: 0,
+    time: "刚刚"
+  }
 ];
