2e49ed88f34e2871e3b4a57526a0a4087efe2e04
aunxxxx <fengaus0905@gmail.com>
2026-05-27 15:47:17 +0800

    Update upload.js

--- js/upload.js
+++ js/upload.js
@@ -1,43 +1,45 @@
-export function initUpload(previewImage) {
-
+function initUpload(previewImage) {
+ 
   const uploadBtn = document.getElementById("uploadBtn");
   const imageInput = document.getElementById("imageInput");
-
+ 
   let compressedImage = "";
-
+ 
   uploadBtn.onclick = () => {
     imageInput.click();
   };
-
+ 
   imageInput.onchange = (e) => {
     const file = e.target.files[0];
     if (!file) return;
-
+ 
     const reader = new FileReader();
-
+ 
     reader.onload = (event) => {
       const img = new Image();
-
+ 
       img.onload = () => {
         const canvas = document.createElement("canvas");
         const ctx = canvas.getContext("2d");
-
+ 
         canvas.width = img.width;
         canvas.height = img.height;
-
+ 
         ctx.drawImage(img, 0, 0);
-
+ 
         compressedImage = canvas.toDataURL("image/jpeg", 0.22);
-
+ 
         previewImage.src = compressedImage;
         previewImage.style.display = "block";
       };
-
+ 
       img.src = event.target.result;
     };
-
+ 
     reader.readAsDataURL(file);
   };
-
+ 
   return () => compressedImage;
 }
+
+window.initUpload = initUpload;
