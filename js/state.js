1446ffe065b77c435122daa322e1f41c71813a31
aunxxxx <fengaus0905@gmail.com>
2026-05-27 15:46:54 +0800

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,13 +1,13 @@
 import { posts as initialPosts } from "./data.js";
-
+ 
 export const state = {
     posts: structuredClone(initialPosts)
 };
-
+ 
 export function getPosts() {
     return state.posts;
 }
-
+ 
 export function updatePost(id, updater) {
     const post = state.posts.find(p => p.id === id);
     if (!post) return;
