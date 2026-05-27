243f99d60d384bfda05017131efb337262935c01
aunxxxx <fengaus0905@gmail.com>
2026-05-27 15:46:42 +0800

    Update statBtn.js

--- js/statBtn.js
+++ js/statBtn.js
@@ -1,19 +1,19 @@
 export function createStatBtn(type, count, onClick) {
-
+ 
   const div = document.createElement("div");
-
+ 
   div.className = `
     stat-btn
     ${type}-btn
     ${type === "like" ? "like-action-btn" : ""}
   `;
-
+ 
   const icons = {
-
+ 
     like: `
-
+ 
       <svg viewBox="0 0 24 24" class="icon like-icon">
-
+ 
         <path
           d="M12.1 20.3l-.1.1-.1-.1C7 16 4 13.2 4 9.8 4 7.2 6 5.2 8.6 5.2c1.5 0 2.9.7 3.8 1.9.9-1.2 2.3-1.9 3.8-1.9C18 5.2 20 7.2 20 9.8c0 3.4-3 6.2-7.9 10.5z"
           fill="none"
@@ -22,15 +22,15 @@ export function createStatBtn(type, count, onClick) {
           stroke-linecap="round"
           stroke-linejoin="round"
         />
-
+ 
       </svg>
-
+ 
     `,
-
+ 
     comment: `
-
+ 
       <svg viewBox="0 0 24 24" class="icon">
-
+ 
         <path
           d="M21 12c0 4.4-4 8-9 8-1.1 0-2.1-.2-3.1-.6L3 21l1.7-4.2C3.7 15.5 3 13.8 3 12c0-4.4 4-8 9-8s9 3.6 9 8z"
           fill="none"
@@ -39,17 +39,17 @@ export function createStatBtn(type, count, onClick) {
           stroke-linecap="round"
           stroke-linejoin="round"
         />
-
+ 
       </svg>
-
+ 
     `
-
+ 
   };
-
+ 
   div.innerHTML = `
-
+ 
     ${icons[type]}
-
+ 
     <span class="${
       type === "like"
         ? "like-count-trigger"
@@ -57,12 +57,12 @@ export function createStatBtn(type, count, onClick) {
     }">
       ${count}
     </span>
-
+ 
   `;
-
+ 
   if (onClick) {
     div.addEventListener("click", onClick);
   }
-
+ 
   return div;
 }
